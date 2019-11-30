d4712abbc08de13c6dc5568c0f7f03259e65206c
Igor Patrick Ponticelli <>
2019-11-30 07:54:22 -0200

    Meta tag adicionada

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <title>Clima</title>
+        <title>Informações Climáticas</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
@@ -16,6 +16,38 @@ and open the template in the editor.
 
         <script src="js/clima.js"></script>
 
+        <!-- Tags Gerais -->
+        <meta NAME="DESCRIPTION" CONTENT="Site de informações Climáticas de Brusque, desenvolvida por Igor P. Ponticelli"/>
+        <meta NAME="ABSTRACT" CONTENT="Informações Climáticas"/>
+        <meta name="keywords" content="clima, temperatura para brusque, tempo para brusque, umidade para brusque, previsao para brusque, por do sol de brusque, clima de brusque, temperatura, umidade, vai chover em brusque, dia para sol em brusque, Igor Patrick Ponticelli, Climas santa catarina, velocidade do vento para brusque"/>
+        <meta NAME="title" CONTENT="Informações Climáticas" />
+        <meta NAME="identifier-url" content="https://igor-clima.herokuapp.com/" />
+        <meta NAME="author" content="Igor Patrick Ponticelli" />
+        <meta NAME="ROBOTS" CONTENT="All" />
+        <meta NAME="RATING" CONTENT="general" />
+        <meta NAME="DISTRIBUTION" CONTENT="global" />
+        <meta NAME="LANGUAGE" CONTENT="pt-br" />
+        <meta name="content-language" content="portuguese" />
+        <meta name="doc-class" content="Completed" /> 
+        <meta name="reply-to" content="igor_p_ponticelli@estudante.sc.senai.br"/>
+
+        <!-- Tags Para rede social Facebook -->
+        <meta property="og:url" content="https://igor-clima.herokuapp.com/" />
+        <meta property="og:locale" content="pt_BR" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Informações Climáticas" />
+        <meta property="og:title" content="Informações Climáticas" />
+        <meta property="og:description" content="Site de informações Climáticas de Brusque, desenvolvida por Igor P. Ponticelli" />
+        <meta property="og:image" content="https://igor-clima.herokuapp.com/img/icones/nascerDoSol.png" />
+        <meta property="og:image:width" content="400" />
+        <meta property="og:image:height" content="120" />
+        <meta property="fb:app_id" content="" />
+
+
+        <!-- Tags Para rede social Twitter -->
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:description" content="Site de informações Climáticas de Brusque, desenvolvida por Igor P. Ponticelli" />
+        <meta name="twitter:title" content="Informações Climáticas" />
     </head>
     <body class="bg-dark">
         <div class="row">
